17ea8c3c9e4b134a464772debdc792d1c11a713f
Synergy Dev <synergycomputing@gmail.com>
2012-10-05 02:13:28 +0000

    ARM: add ARCH_ARM_HIGH_OPTIMIZATION This enables -O3 instead of -Os for thumb cflags intended for devices which don't have compatibility issues with -O3 everywhere.
    Conflicts:
    
    	core/combo/TARGET_linux-arm.mk
    
    Change-Id: Id5d0212685139462f269a3d74713137741ea4cad

--- core/combo/TARGET_linux-arm.mk
+++ core/combo/TARGET_linux-arm.mk
@@ -76,10 +76,21 @@ TARGET_arm_CFLAGS :=    -O2 \
 # as a 'hint'. If thumb is not enabled, these files are just
 # compiled as ARM.
 ifeq ($(ARCH_ARM_HAVE_THUMB_SUPPORT),true)
+    ifeq ($(ARCH_ARM_HIGH_OPTIMIZATION),true)
+TARGET_thumb_CFLAGS :=  -mthumb \
+                        -O3 \
+                        -fomit-frame-pointer \
+                        -fstrict-aliasing \
+                        -Wstrict-aliasing=2 \
+                        -Werror=strict-aliasing
+    else
 TARGET_thumb_CFLAGS :=  -mthumb \
                         -Os \
                         -fomit-frame-pointer \
-                        -fno-strict-aliasing
+                        -fstrict-aliasing \
+                        -Wstrict-aliasing=2 \
+                        -Werror=strict-aliasing
+    endif
 else
 TARGET_thumb_CFLAGS := $(TARGET_arm_CFLAGS)
 endif
