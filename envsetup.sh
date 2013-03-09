d3540f01e5a67d533b9877416a53da36c8de1db7
Randall Rushing <randall.rushing@gmail.com>
2013-03-09 00:09:24 -0800

    Modify build for Linaro 4.7 toolchain support
    PS3:
    - Sorry guys, i can't get the optional flag going
      Better get those devices linaro 4.7 compliant!
    
    Change-Id: I4839ef5d2fd9a51dba2cf9f46714e395e66b9694

--- envsetup.sh
+++ envsetup.sh
@@ -125,7 +125,7 @@ function setpaths()
     case $ARCH in
         x86) toolchaindir=x86/i686-linux-android-4.6/bin
             ;;
-        arm) toolchaindir=arm/arm-linux-androideabi-$targetgccversion/bin
+        arm) toolchaindir=arm/linaro-$targetgccversion/bin
             ;;
         mips) toolchaindir=mips/mipsel-linux-android-4.6/bin
             ;;
@@ -141,7 +141,7 @@ function setpaths()
     unset ARM_EABI_TOOLCHAIN ARM_EABI_TOOLCHAIN_PATH
     case $ARCH in
         arm)
-            toolchaindir=arm/arm-eabi-$targetgccversion/bin
+            toolchaindir=arm/linaro-$targetgccversion/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ARM_EABI_TOOLCHAIN_PATH=":$gccprebuiltdir/$toolchaindir"
