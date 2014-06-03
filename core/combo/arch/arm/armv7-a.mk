decb1fbdb955ddce2c15daf952a86ed81cca5a30
timduru <timduru@gmail.com>
2014-06-03 13:46:19 +0200

    partial merge 5931464351b0ffdf733df053f5dd0caaf98c1f14
    Change-Id: I98e4ae38710777a3bbfa0039d6e54df7c6cc3a3f

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -4,13 +4,36 @@
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
 
+ifeq ($(strip $(TARGET_CPU_VARIANT)), cortex-a15)
+	arch_variant_cflags := -mcpu=cortex-a15
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a9)
+	arch_variant_cflags := -mcpu=cortex-a9
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+	arch_variant_cflags := -mcpu=cortex-a8
+else
+ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a7)
+	arch_variant_cflags := -mcpu=cortex-a7
+else
+	arch_variant_cflags := -march=armv7-a
+endif
+endif
+endif
+endif
+
+
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
-arch_variant_cflags := \
-    -march=armv7-a \
+arch_variant_cflags += \
     -mfloat-abi=softfp \
     -mfpu=vfpv3-d16
 
+ifneq ($(strip $(TARGET_CPU_VARIANT)),cortex-a8)
+arch_variant_ldflags := \
+	-Wl,--no-fix-cortex-a8
+else
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
+endif
