25bfc6ea24b7b1fbd60f97d309079c0253b1c069
timduru <timduru@gmail.com>
2016-03-04 16:12:16 +0100

    non-neon tests

--- core/combo/arch/arm/armv7-a.mk
+++ core/combo/arch/arm/armv7-a.mk
@@ -4,13 +4,54 @@
 ARCH_ARM_HAVE_ARMV7A            := true
 ARCH_ARM_HAVE_VFP               := true
 
+ifeq ($(strip $(TARGET_ARCH_VARIANT_FPU)),neon)
+ARCH_ARM_HAVE_NEON              := true
+else
+ARCH_ARM_HAVE_NEON              :=
+endif
+
+
+ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
+        # TODO: krait is not a cortex-a15, we set the variant to cortex-a15 so that
+        #       hardware divide operations are generated. This should be removed and a
+        #       krait CPU variant added to GCC. For clang we specify -mcpu for krait in
+        #       core/clang/arm.mk.
+        arch_variant_cflags := -mcpu=cortex-a15
+
+        # Fake an ARM compiler flag as these processors support LPAE which GCC/clang
+        # don't advertise.
+        arch_variant_cflags += -D__ARM_FEATURE_LPAE=1
+        arch_variant_ldflags := \
+                -Wl,--no-fix-cortex-a8
+else
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
+        arch_variant_cflags := -mcpu=cortex-a9
+        arch_variant_ldflags := \
+                -Wl,--no-fix-cortex-a8
+else
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
+        arch_variant_cflags := -mcpu=cortex-a8
+        arch_variant_ldflags := \
+                -Wl,--fix-cortex-a8
+else
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
+        arch_variant_cflags := -mcpu=cortex-a7
+        arch_variant_ldflags := \
+                -Wl,--no-fix-cortex-a8
+else
+        arch_variant_cflags := -march=armv7-a
+        # Generic ARM might be a Cortex A8 -- better safe than sorry
+        arch_variant_ldflags := \
+                -Wl,--fix-cortex-a8
+endif
+endif
+endif
+endif
+
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
-arch_variant_cflags := \
-    -march=armv7-a \
+arch_variant_cflags += \
     -mfloat-abi=softfp \
     -mfpu=vfpv3-d16
 
-arch_variant_ldflags := \
-	-Wl,--fix-cortex-a8
